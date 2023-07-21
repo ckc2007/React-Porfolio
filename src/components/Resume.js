45526d5f51cf8b83e23c860281b94ff923574bd0
ckc2007 <ckc2007@gmail.com>
2023-07-21 19:49:17 -0400

    added Resume component

---
+++ src/components/Resume.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Resume() {
+  return (
+    // <!-- resume section here -->
+    <section id="resume" class="resume">
+      <h2>Resume</h2>
+      {/* <!-- image of the pdf will show on click--> */}
+      <ul>
+        <a href="./assets/docs/cv.pdf" target="_blank">
+          <li>CV</li>
+        </a>
+        <a href="./assets/docs/bio.pdf" target="_blank">
+          <li>Bio</li>
+        </a>
+      </ul>
+    </section>
+  );
+}
+
+export default Resume;
