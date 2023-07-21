02bd466038af1fb1fbaa3fc5d873e0031945a314
ckc2007 <ckc2007@gmail.com>
2023-07-21 19:25:01 -0400

    added App function

--- src/App.js
+++ src/App.js
@@ -3,3 +3,14 @@ import Header from "./components/Header";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
 
+function App() {
+  return (
+    <div>
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
