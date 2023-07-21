43e870d6eb6baa81d288af0efb8de4a8b18e340a
ckc2007 <ckc2007@gmail.com>
2023-07-21 19:52:19 -0400

    import Main into App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
-import Header from "./components/Header";
 import Main from "./components/Main";
-import Footer from "./components/Footer";
+
+// import "./assets/css/reset.css";
+// import "./assets/css/style.css";
 
 function App() {
   return (
