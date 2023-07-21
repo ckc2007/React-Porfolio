5aa86fea6c70ec2621d2cc8150143fa0220f02a0
ckc2007 <ckc2007@gmail.com>
2023-07-21 19:58:05 -0400

    added Nav to Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Nav from "./Nav";
 // import any necessary files from ../images/filename;
 
 function Header() {
@@ -9,28 +10,14 @@ function Header() {
           Chris <span id="chorney">Chorney</span>
         </h1>
         <img
-          class="display-inline"
+          className="display-inline"
           src="./assets/images/chorney bio pic.jpeg"
           alt="chris chorney in an aloha shirt with his cello by a fountain"
         />
         <p id="tagline">Music. Photography. Web Design.</p>
       </div>
-      <nav>
-        <ul>
-          <li class="display-inline">
-            <a href="#about-me">About Me</a>
-          </li>
-          <li class="display-inline">
-            <a href="#work">Work</a>
-          </li>
-          <li class="display-inline">
-            <a href="#resume">Resume</a>
-          </li>
-          <li class="display-inline">
-            <a href="#contact-me">Contact Me</a>
-          </li>
-        </ul>
-      </nav>
+      {/* import from Nav component */}
+      <Nav />
     </header>
   );
 }
