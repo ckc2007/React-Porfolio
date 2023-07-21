2129abb91133619c6ae3980d381906d2fe4e6bbd
ckc2007 <ckc2007@gmail.com>
2023-07-21 19:44:06 -0400

    created components and about me

---
+++ src/components/AboutMe.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function AboutMe() {
+  return (
+    <section id="about-me" className="about-me">
+      <h2>About Me</h2>
+      <p>
+        Christopher Chorney is a cello player and multi-instrumentalist hailing
+        from Honolulu, Hawaii who has played numerous Grammy-Award Winning
+        tracks. He is the owner of Analog Rock Machine Studio and has produced
+        albums for a diverse array of local musicians. His interests include
+        three-dimensional audio, web3, digital artwork, and full-stack web
+        development. He currently resides in Richmond Virginia.
+      </p>
+    </section>
+  );
+}
+
+export default AboutMe;
