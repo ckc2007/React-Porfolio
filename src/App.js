dc6af35afb523ab21d1263bbfdab00ccf06982d3
ckc2007 <ckc2007@gmail.com>
2023-07-21 20:51:12 -0400

    working app yay

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from "react";
 import Main from "./components/Main";
 
-// import "./assets/css/reset.css";
-// import "./assets/css/style.css";
 
 function App() {
   return (
     <div>
-      <Header />
       <Main />
-      <Footer />
     </div>
   );
 }
