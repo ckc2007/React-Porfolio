ee2195b161a5fb84f5f0b2648669b5eed9b29662
ckc2007 <ckc2007@gmail.com>
2023-07-21 23:06:40 -0400

    added download capability to bio link plus notes

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -22,8 +22,10 @@ function ContactForm() {
   function handleBlur(e) {
     const { name, value } = e.target;
     if (!value.trim()) {
+      // [name]: "This field is required": This uses computed property names to dynamically set the property name in the object. name is a variable holding the name of the form field (e.g., "name", "email", or "message"). By wrapping name in square brackets [name], it becomes a dynamic property name based on the value of the name variable.
       setErrors({ ...errors, [name]: "This field is required" });
     } else if (name === "email" && !validateEmail(value)) {
+      // setErrors({...errors, [name]: "This field is required" }): This sets the errors state with an updated object. It takes the shallow copy of the existing errors object and adds/updates a property with the name as the key and the value of "This field is required". If a property with the same name already exists in the errors state, it will be replaced with the new value.
       setErrors({ ...errors, email: "Invalid email address" });
     } else {
       setErrors({ ...errors, [name]: "" });
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,18 +1,44 @@
 import React from "react";
 
 function Resume() {
+  const handleDownload = (e, fileName) => {
+    e.preventDefault();
+    if (window.confirm(`Do you want to download the ${fileName}?`)) {
+      const resumeUrl = process.env.PUBLIC_URL + `/docs/${fileName}.pdf`;
+      // creates a new anchor <a> in the DOM
+      const link = document.createElement("a");
+      link.href = resumeUrl;
+      // When you set thedownload attribute on an anchor (<a>`) element and provide a value, the browser will download the linked file with the filename you specified.
+      link.setAttribute("download", `${fileName}.pdf`);
+      //  programmatically triggers a click event on the anchor element. When the user clicks the link, the browser normally navigates to the URL specified in the href attribute of the anchor. However, when the download attribute is set, it changes the behavior, and the browser initiates a file download instead of navigating to the URL.
+      // By using link.click();, we are simulating a click event on the anchor element. When this line is executed, it's as if the user clicked on the anchor link, and the browser will download the file associated with the href attribute. This technique is commonly used to trigger file downloads programmatically in JavaScript.
+      link.click();
+    }
+  };
+
   return (
-    // <!-- resume section here -->
     <section id="resume" className="resume">
       <h2>Resume</h2>
-      {/* <!-- image of the pdf will show on click--> */}
+      {/* List of PDFs */}
       <ul>
-        <a href="/docs/cv.pdf" target="_blank">
-          <li>CV</li>
-        </a>
-        <a href="/docs/bio.pdf" target="_blank">
-          <li>Bio</li>
-        </a>
+        <li>
+          <a
+            href="/docs/cv.pdf"
+            target="_blank"
+            onClick={(e) => handleDownload(e, "cv")}
+          >
+            CV
+          </a>
+        </li>
+        <li>
+          <a
+            href="/docs/bio.pdf"
+            target="_blank"
+            onClick={(e) => handleDownload(e, "bio")}
+          >
+            Bio
+          </a>
+        </li>
       </ul>
     </section>
   );
