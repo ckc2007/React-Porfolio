68ff0cb4cc23f4e9f42b78a7925856042b129e03
ckc2007 <ckc2007@gmail.com>
2023-07-21 20:35:49 -0400

    added docs to resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -7,10 +7,10 @@ function Resume() {
       <h2>Resume</h2>
       {/* <!-- image of the pdf will show on click--> */}
       <ul>
-        <a href="./assets/docs/cv.pdf" target="_blank">
+        <a href="/docs/cv.pdf" target="_blank">
           <li>CV</li>
         </a>
-        <a href="./assets/docs/bio.pdf" target="_blank">
+        <a href="/docs/bio.pdf" target="_blank">
           <li>Bio</li>
         </a>
       </ul>
